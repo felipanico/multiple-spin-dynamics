da4e1298b1e3eacdfcfd0969cff08967592635da
Felipe Rocha <felipesp.rocha@gmail.com>
2021-03-07 17:15:44 -0300

    add plot3dPositions()

--- magnetization.py
+++ magnetization.py
@@ -4,11 +4,11 @@ import sys
 from matplotlib import pyplot as plt
 
 # global variables
-n = 1000
+n = 300
 h = 0.05
 Nx = 2
 Ny = 2
-H = np.array([0,0,1])
+H = np.array([0,0,10])
 position = 1.0 / np.sqrt(3.0)
 
 # begin functions
@@ -38,12 +38,7 @@ def createSpinLattice():
 	for i in range(Nx):
 		for j in range(Ny):
 			k = i + j
-			spinLattice[i][j] = [i+1, j+1, k+1]
-
-def createSpinPosition():
-	for i in range(Nx):
-		for j in range(Ny):
-			spinPosition[i][j] = [i, j, 0]
+			spinLattice[i][j] = [i + 1, j + 1, k + 1]
 
 def plotMagnetization(mx, my, mz):
 	plt.plot(mx, label = 'mx')
@@ -60,13 +55,13 @@ def plotInitialPositions():
 	sx = spinLattice[:,:,0]
 	sy = spinLattice[:,:,1]
 	sz = spinLattice[:,:,2]
+
+	print(spinLattice)
+	
 	fig, ax = plt.subplots(figsize=(7,7))
 	width=0.003
 	ax.quiver(sx,sy,sz, pivot='mid',width=width)
-	ax.xaxis.set_ticks([])
-	ax.yaxis.set_ticks([])
-	ax.set_aspect('equal')
-	plt.show()
+	#plt.show()
 
 def plotFinalPositions():
 	sx = spinLattice[:,:,0]
@@ -74,20 +69,20 @@ def plotFinalPositions():
 	sz = spinLattice[:,:,2]
 	fig2, bx = plt.subplots(figsize=(7,7))
 	width=0.003
-	bx.quiver(sx,sy,sz, pivot='mid', width=width)
-	bx.xaxis.set_ticks([])
-	bx.yaxis.set_ticks([])
-	bx.set_aspect('equal')
+	bx.quiver(sx,sy,sz, pivot='mid',width=width)
 	plt.show()
 
-def createFinalSpinLattice(size):
-	finalSpinLattice = np.zeros((size,2,3),np.float64)
+def plot3dPositions(sx,sy,sz):
+	fig3 = plt.figure()
+	cx = fig3.gca(projection='3d')
 	
-	for k in range(size):
-		finalSpinLattice[k][0] = mx[k], my[k], mz[k]
-		finalSpinLattice[k][1] = mx[k+1], my[k+1], mz[k+1]
-	
-	return finalSpinLattice
+	x = 0
+	y = 0
+	z = 0
+
+	cx.quiver(x,y,z, sx,sy,sz, length=0.03, normalize=True)
+	plt.show()
+
 #end functions
 
 #main program
@@ -96,18 +91,28 @@ my = np.zeros((Nx*Ny,n-1))
 mz = np.zeros((Nx*Ny,n-1))
 
 spinLattice = np.zeros((Nx+2,Ny+2,3),np.float64)[1:Nx+1,1:Ny+1,:]
-spinPosition = np.zeros((Nx+2,Ny+2,3),np.float64)[1:Nx+1,1:Ny+1,:]
+spinLatticeFinal = np.zeros((Nx+2,Ny+2,3),np.float64)[1:Nx+1,1:Ny+1,:]
 createSpinLattice()
+#plotInitialPositions()
 
 for t in range(n-1):
 	i = 0
 	for x in range(Nx):
 		for y in range(Ny):
 			spin = euler(spinLattice[x][y], x, y)
+			spinLatticeFinal[x][y] = spin
 			mx[i][t] = spin[0]
 			my[i][t] = spin[1]
 			mz[i][t] = spin[2]
 			i = i+1
 			
-spinIndex = 0 #index of spin for magnetization plot
-plotMagnetization(mx[spinIndex],my[spinIndex],mz[spinIndex])
+#print(spinLatticeFinal)
+
+plotFinalPositions()
+
+#spinNumber = 0
+#sx = mx[spinNumber][t-1]
+#sy = my[spinNumber][t-1]
+#sz = mz[spinNumber][t-1]
+#plot3dPositions(sx, sy, sz)
+#plotMagnetization(mx[spinNumber],my[spinNumber],mz[spinNumber])
