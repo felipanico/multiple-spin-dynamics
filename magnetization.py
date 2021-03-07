81bda41e31445a10bb138b176bc92d4880f9e58f
Felipe Rocha <felipesp.rocha@gmail.com>
2021-03-07 01:46:13 -0300

    fix magnetization plot and update gitignore

--- magnetization.py
+++ magnetization.py
@@ -4,27 +4,22 @@ import sys
 from matplotlib import pyplot as plt
 
 # global variables
-n = 100
+n = 1000
 h = 0.05
-Nx = 5
-Ny = 5
+Nx = 2
+Ny = 2
 H = np.array([0,0,1])
 position = 1.0 / np.sqrt(3.0)
 
 # begin functions
-mx = []
-my = []
-mz = []
 def euler(spin, x, y):
 	result = derivate(spin, Heff(spin,H))
 	spin[0] = spin[0] + h*result[0]
 	spin[1] = spin[1] + h*result[1]
 	spin[2] = spin[2] + h*result[2]
 	spinLattice[x][y] = spin
-	mx.append(spin[0])
-	my.append(spin[1])
-	mz.append(spin[2])
-
+	return spin
+	
 def giromagneticRatio():
 	k = 1 #numeric value is 1.76*10**(11)
 	u0 = 1 #numeric value is 1.2566*10**(-6)
@@ -40,28 +35,34 @@ def Heff(S,H):
 	return np.add(H, lamb * np.cross(S,H))
 
 def createSpinLattice():
-	for x in range(Nx):
-		for y in range(Ny):
-			spinLattice[x][y] = [x,y,0]
-	
-	return spinLattice
+	for i in range(Nx):
+		for j in range(Ny):
+			k = i + j
+			spinLattice[i][j] = [i+1, j+1, k+1]
 
+def createSpinPosition():
+	for i in range(Nx):
+		for j in range(Ny):
+			spinPosition[i][j] = [i, j, 0]
 
-def plotM(mx, my, mz):
+def plotMagnetization(mx, my, mz):
 	plt.plot(mx, label = 'mx')
 	plt.plot(my, label = 'my')
 	plt.plot(mz, label = 'mz')
 	plt.title('Magnetization')
 	plt.show()
 
+
+sx = []
+sy = []
+sz = []
 def plotInitialPositions():
 	sx = spinLattice[:,:,0]
 	sy = spinLattice[:,:,1]
+	sz = spinLattice[:,:,2]
 	fig, ax = plt.subplots(figsize=(7,7))
 	width=0.003
-	scale=1.0
-	ax.quiver(sx,sy, pivot='mid',width=width)
-
+	ax.quiver(sx,sy,sz, pivot='mid',width=width)
 	ax.xaxis.set_ticks([])
 	ax.yaxis.set_ticks([])
 	ax.set_aspect('equal')
@@ -70,27 +71,43 @@ def plotInitialPositions():
 def plotFinalPositions():
 	sx = spinLattice[:,:,0]
 	sy = spinLattice[:,:,1]
+	sz = spinLattice[:,:,2]
 	fig2, bx = plt.subplots(figsize=(7,7))
 	width=0.003
-	scale=1.0
-	bx.quiver(sx,sy, pivot='mid',width=width)
+	bx.quiver(sx,sy,sz, pivot='mid', width=width)
 	bx.xaxis.set_ticks([])
 	bx.yaxis.set_ticks([])
 	bx.set_aspect('equal')
-	plt.show()		
+	plt.show()
 
+def createFinalSpinLattice(size):
+	finalSpinLattice = np.zeros((size,2,3),np.float64)
+	
+	for k in range(size):
+		finalSpinLattice[k][0] = mx[k], my[k], mz[k]
+		finalSpinLattice[k][1] = mx[k+1], my[k+1], mz[k+1]
+	
+	return finalSpinLattice
 #end functions
 
 #main program
+mx = np.zeros((Nx*Ny,n-1))
+my = np.zeros((Nx*Ny,n-1))
+mz = np.zeros((Nx*Ny,n-1))
+
 spinLattice = np.zeros((Nx+2,Ny+2,3),np.float64)[1:Nx+1,1:Ny+1,:]
+spinPosition = np.zeros((Nx+2,Ny+2,3),np.float64)[1:Nx+1,1:Ny+1,:]
 createSpinLattice()
 
-plotInitialPositions()
-
 for t in range(n-1):
+	i = 0
 	for x in range(Nx):
 		for y in range(Ny):
-			euler(spinLattice[x][y], x, y)
-
-plotFinalPositions()
-plotM(mx,my,mz)
+			spin = euler(spinLattice[x][y], x, y)
+			mx[i][t] = spin[0]
+			my[i][t] = spin[1]
+			mz[i][t] = spin[2]
+			i = i+1
+			
+spinIndex = 0 #index of spin for magnetization plot
+plotMagnetization(mx[spinIndex],my[spinIndex],mz[spinIndex])
