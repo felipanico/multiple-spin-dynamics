fix normalization y

plot.py
@@ -12,17 +12,19 @@ def individualMagnetization(mx, my, mz):
 
 def spins2D(spinLattice):
 	fig, ax = plt.subplots(figsize=(6,6))
-	cmap=plt.get_cmap('coolwarm_r')
-	width=0.0025
+	cmap = plt.get_cmap('coolwarm_r')
+	width = 0.0025
 	zmin = -0.001
 	zmax = 0.001
 	interpolation='nearest'
 
 	x = spinLattice[:,:,0]
 	y = spinLattice[:,:,1]
-	x = x / np.sqrt(x**2 + y**2)
-	y = y / np.sqrt(y**2 + y**2)
 	z = spinLattice[:,:,2]
+	x = x / np.sqrt(x**2 + y**2)
+	y = y / np.sqrt(x**2 + y**2)
+	#y = y / np.sqrt(y**2 + y**2)
+	
 
 	im=ax.imshow(z, interpolation=interpolation, cmap = cmap, origin='lower')
 	ax.quiver(x, y, pivot='mid', zorder=2, width=width, scale=5.0 ,scale_units='xy', headwidth=6, headlength=8)