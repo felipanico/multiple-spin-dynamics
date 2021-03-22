10d431b62f7bdf60015bfcd848e39c2dcd0ebd95
Felipe Rocha <felipesp.rocha@gmail.com>
2021-03-21 21:55:39 -0300

    rolback euler and Heff functions

--- calc.py
+++ calc.py
@@ -2,8 +2,8 @@ import numpy as np
 import math
 import sys
 
-def euler(H, spinLattice, spin, x, y, h, spinTotal, index, mx, my, mz):
-	H = Heff(spin, spinLattice , H, spinTotal, index, mx, my, mz)
+def euler(H, spinLattice, spin, x, y, h):
+	H = Heff(spin, H)
 	result = derivate(spin, H)
 	spin[0] = spin[0] + h*result[0]
 	spin[1] = spin[1] + h*result[1]
@@ -22,87 +22,5 @@ def derivate(S,H):
 	return giromagneticRatio()*(np.cross(S,H))
 
 def Heff(S,H):
-    lamb = 0.1 #lambda is reserved world
-	return np.add(H, lamb * np.cross(S,H))
-
-def Heff(S,H):
-	lamb = 0.1 #lambda is reserved world
-	
-	J = 1
-	A = 1
-	B = 1
-	C = 1
-	
-	S[0] = A*S[0]
-	S[1] = B*S[1]
-	S[2] = C*S[2]
-	
-	sx = np.zeros((spinTotal))
-	sy = np.zeros((spinTotal))
-	sz = np.zeros((spinTotal))
-
-	for k in range(spinTotal):
-		sx[k] = mx[k][index-1]
-		sy[k] = my[k][index-1]
-		sz[k] = mz[k][index-1]
-	
-	term1 = np.add(exchangeInteraction(sx, sy, sz, index, k), S) 
-	term1 = np.add(term1, H)
-	
-	term2 = lamb * np.cross(term1, S)
-
-	Heff = np.add(term1, term2)
-
-	return Heff
-
-def exchangeInteraction(sx, sy, sz, index, spinTotal):
-	J = 1
-	
-	for k in range(spinTotal):
-		isSpinInEdge = 0		
-		
-		if (k == 0):
-			isSpinInEdge = 1
-			previousSpinIndex = spinTotal
-			nextSpinIndex = k+1	
-		
-		if (k == spinTotal):
-			isSpinInEdge = 1
-			nextSpinIndex = 0
-			previousSpinIndex = k-1
-
-		if (isSpinInEdge == 0):
-			previousSpinIndex = k-1	
-			nextSpinIndex = k+1	
-
-		spinInteractionX = sx[previousSpinIndex] + sx[k] + sx[nextSpinIndex]
-		spinInteractionY = sy[previousSpinIndex] + sy[k] + sy[nextSpinIndex]
-		spinInteractionZ = sz[previousSpinIndex] + sz[k] + sz[nextSpinIndex]
-		
-		spinA = [sx[previousSpinIndex], sx[previousSpinIndex], sx[previousSpinIndex]]
-		
-		spinB = [sx[k], sy[k], sz[k]]
-
-		spinC = [sx[nextSpinIndex], sx[nextSpinIndex], sx[nextSpinIndex]]
-		
-		#print('spinA', spinA)
-
-		#print('spinB', spinB)
-
-		#print('spinC', spinC)
-
-		#sys.exit()
-		
-		spinInteraction = [spinInteractionX, spinInteractionY, spinInteractionZ]
-		
-	#if (math.isnan(spinInteractionX)):
-		#print(previousSpinIndex, nextSpinIndex)
-		#print(sx)
-		#sys.exit()
-
-	#print(spinInteraction)
-
-	#sys.exit()
-	
-	return J*spinInteraction
-	 
+	_lambda = 0.1
+	return np.add(H, _lambda * np.cross(S,H))
