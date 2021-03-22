fix normalization x and y

lattice.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import random
 
 def createSpinLattice(n,Nx,Ny,mx,my,mz):
     spinLattice = np.zeros((Nx+2,Ny+2,3),np.float64)[1:Nx+1,1:Ny+1,:]
@@ -9,11 +10,12 @@ def createSpinLattice(n,Nx,Ny,mx,my,mz):
         u = 0
         for x in range(Nx):
             for y in range(Ny):
-                magx = x*normalization + normalization
-                magy = y*normalization + normalization
-                magz = 0
                 
+                magx = random.uniform(0.01, 0.06)
+                magy = random.uniform(0.01, 0.06) 
+                magz = random.uniform(0.01, 0.06) 
                 spin = [magx, magy, magz]
+                
                 spinLattice[x][y] = spin
                 
                 mx[u][v] = spin[0]
@@ -21,6 +23,11 @@ def createSpinLattice(n,Nx,Ny,mx,my,mz):
                 mz[u][v] = spin[2]
                 
                 u = u+1
+    
+    #print(mx)
+    #print(my)
+    #print(mz)
+    #sys.exit()
     
     return spinLattice
 

magnetization.py
@@ -4,11 +4,11 @@ import plot
 import lattice
 import calc
 
-n = 2000
+n = 1000
 h = 0.05
 Nx = 5
 Ny = 5
-H = np.array([0,0,1])
+H = np.array([0,0,3])
 
 sx = []
 sy = []
@@ -22,7 +22,7 @@ spinLattice = lattice.createSpinLattice(n,Nx,Ny,mx,my,mz)
 
 spinTotal = Nx*Ny
 
-plot.spins2D(spinLattice)
+plot.spins2D(spinLattice, 5.0, 0.01, 0.05)
 
 for t in range(n-1):
 	i = 0
@@ -34,5 +34,5 @@ for t in range(n-1):
 			mz[i][t] = spin[2]
 			i = i+1
 
-plot.spins2D(spinLattice)
+plot.spins2D(spinLattice, 0.005, -1, 1)
 

plot.py
@@ -10,23 +10,30 @@ def individualMagnetization(mx, my, mz):
 	plt.title('Magnetization')
 	plt.show()
 
-def spins2D(spinLattice):
+def spins2D(spinLattice, scale, zmin, zmax):
 	fig, ax = plt.subplots(figsize=(6,6))
 	cmap = plt.get_cmap('coolwarm_r')
 	width = 0.0025
-	zmin = 0
-	zmax = 10
 	interpolation='nearest'
 
 	x = spinLattice[:,:,0]
 	y = spinLattice[:,:,1]
 	z = spinLattice[:,:,2]
-	x = x / np.sqrt(x**2 + y**2)
-	y = y / np.sqrt(x**2 + y**2)
+
+	normalization = (10)**(-2)
+
+	xNormalized = (x / np.sqrt(x**2 + y**2 + z**2))
+	yNormalized = (y / np.sqrt(x**2 + y**2 + z**2))
 	
-	im=ax.imshow(z, interpolation=interpolation, cmap = cmap, origin='lower', vmin=0,vmax=1)
-	ax.quiver(x, y, pivot='mid', zorder=2, width=width, scale=5.0 ,scale_units='xy', headwidth=6, headlength=8)
-	fig.colorbar(im, label=r'$m_z$',orientation='vertical')
+	print('x')
+	print(xNormalized)
+	
+	print('z')
+	print(z)
+
+	im=ax.imshow(z, interpolation=interpolation, cmap = cmap, origin='lower', vmin=zmin, vmax=zmax)
+	ax.quiver(xNormalized, yNormalized, pivot='mid', zorder=2, width=width, scale=scale ,scale_units='xy', headwidth=6, headlength=8)
+	fig.colorbar(im, label=r'$m_z$', orientation='vertical')
 	
 	plt.show()
 	